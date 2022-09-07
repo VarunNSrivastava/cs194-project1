0f5f4c3a7ac604b27f557ee3f2c025ee47eb5823
Varun Srivastava <varun.neal@berkeley.edu>
2022-09-06 19:35:14 -0700

    remove tiffs

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# CS194-26 (CS294-26): Project 1 starter Python code
+# CS194-26 (CS294-26): Project 1
+# author: Varun Neal Srivastava
 
-# these are just some suggested libraries
-# instead of scikit-image you could use matplotlib and opencv to read, write, and display images
-import math
 from time import time
 import numpy as np
 import skimage as sk
 import skimage.io as skio
-from skimage.transform import rescale, rotate
+from skimage.transform import rescale
 import matplotlib
 import os
-from random import randint
 
 matplotlib.use('TkAgg')
 skio.use_plugin('matplotlib')
@@ -23,7 +20,7 @@ def NCC(v1, v2):
     return np.dot(nv1, nv2)
 
 
-def sample(guess, res, size):
+def lattice(guess, res, size):
     # returns all points in a +/-size window around guess
     # where size is upscaled based on resolution
     return [
@@ -35,10 +32,7 @@ def sample(guess, res, size):
 
 class Image:
     def __init__(self, file):
-        time_0 = time()
-
         self.image = skio.imread(file)
-        print(f"imread takes {time() - time_0} seconds")
 
         self.image_height, self.image_width = self.image.shape
 
@@ -55,20 +49,11 @@ class Image:
 
 
     def get_image(self, center, res=1):
-
-        # print(center[0] - self.top_height)
-        # print(center[0] + self.bottom_height)
-        # print(center[1] - self.left_width)
-        # print(center[1] + self.right_width)
-
         img = self.image[center[0] - self.top_height: center[0] + self.bottom_height,
               center[1] - self.left_width: center[1] + self.right_width]
         return rescale(img, res, anti_aliasing=False)
 
     def get_score(self, center1, center2, res=1):
-        # print(f"Center 1 is {center1}")
-        # print(f"Center 2 is {center2}")
-
         img1 = self.get_image(center1, res=res)
         img2 = self.get_image(center2, res=res)
 
@@ -83,7 +68,7 @@ class Image:
         if res > 1:
             res = 1
         # gets lattice of points near guess
-        points = sample(guess, res, 2)
+        points = lattice(guess, res, 2)
         for point in points:
             print(f"{point[0]} \t {point[1]}")
 
@@ -105,14 +90,19 @@ class Image:
         self.right_width = min([self.image_width - center[1] for center in centers])
 
     def align(self):
+        # calculate the alignments for each image
+
         # somewhat arbitrary starting point for coarsest resolution
         res = 1 / (0.014 * self.image_width + 10)
 
         self.green_center = self.calc_center(self.green_center, res)
         self.red_center = self.calc_center(self.red_center, res)
+
+        # cropping images to be the same size
         self.calc_dimensions()
 
     def out(self):
+        # stack the images on top of each other
         r = sk.util.img_as_ubyte(self.get_image(self.red_center))
         g = sk.util.img_as_ubyte(self.get_image(self.green_center))
         b = sk.util.img_as_ubyte(self.get_image(self.blue_center))
@@ -121,6 +111,7 @@ class Image:
 
 def main():
     for f in os.listdir("lib"):
+        
         time_0 = time()
         img = Image(os.path.join("lib", f))
         img.align()
@@ -139,8 +130,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # img = Image("lib/emir.tiff")
-    # img.align()
-    # skio.imshow(img.out())
-    # skio.show()
     main()
